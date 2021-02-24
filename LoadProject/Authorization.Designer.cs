b2668181142edbbc457c0c9b7bd5e148e4b1e2c6
MashaArchipenko <masha.archipenko.99@mail.ru>
2021-02-25 01:11:13 +0300

    create ecryption and write to file a password

--- LoadProject/Authorization.Designer.cs
+++ LoadProject/Authorization.Designer.cs
@@ -28,89 +28,90 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.loginTextBox = new System.Windows.Forms.TextBox();
-            this.passTextBox = new System.Windows.Forms.TextBox();
-            this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
-            this.InputBtn = new System.Windows.Forms.Button();
-            this.ExitBtn = new System.Windows.Forms.Button();
-            this.SuspendLayout();
-            // 
-            // loginTextBox
-            // 
-            this.loginTextBox.Location = new System.Drawing.Point(33, 50);
-            this.loginTextBox.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
-            this.loginTextBox.Name = "loginTextBox";
-            this.loginTextBox.Size = new System.Drawing.Size(132, 30);
-            this.loginTextBox.TabIndex = 0;
-            // 
-            // passTextBox
-            // 
-            this.passTextBox.Location = new System.Drawing.Point(33, 137);
-            this.passTextBox.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
-            this.passTextBox.Name = "passTextBox";
-            this.passTextBox.PasswordChar = '*';
-            this.passTextBox.Size = new System.Drawing.Size(132, 30);
-            this.passTextBox.TabIndex = 1;
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(73, 22);
-            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(50, 23);
-            this.label1.TabIndex = 2;
-            this.label1.Text = "Логин";
-            // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(73, 98);
-            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(58, 23);
-            this.label2.TabIndex = 3;
-            this.label2.Text = "Пароль";
-            // 
-            // InputBtn
-            // 
-            this.InputBtn.Location = new System.Drawing.Point(12, 191);
-            this.InputBtn.Name = "InputBtn";
-            this.InputBtn.Size = new System.Drawing.Size(75, 31);
-            this.InputBtn.TabIndex = 4;
-            this.InputBtn.Text = "Войти";
-            this.InputBtn.UseVisualStyleBackColor = true;
-            this.InputBtn.Click += new System.EventHandler(this.InputBtnClick);
-            // 
-            // ExitBtn
-            // 
-            this.ExitBtn.Location = new System.Drawing.Point(108, 191);
-            this.ExitBtn.Name = "ExitBtn";
-            this.ExitBtn.Size = new System.Drawing.Size(75, 31);
-            this.ExitBtn.TabIndex = 5;
-            this.ExitBtn.Text = "Выход";
-            this.ExitBtn.UseVisualStyleBackColor = true;
-            this.ExitBtn.Click += new System.EventHandler(this.ExitBtnClick);
-            // 
-            // Authorization
-            // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 23F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(207, 245);
-            this.ControlBox = false;
-            this.Controls.Add(this.ExitBtn);
-            this.Controls.Add(this.InputBtn);
-            this.Controls.Add(this.label2);
-            this.Controls.Add(this.label1);
-            this.Controls.Add(this.passTextBox);
-            this.Controls.Add(this.loginTextBox);
-            this.Font = new System.Drawing.Font("Segoe Print", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
-            this.Name = "Authorization";
-            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
-            this.ResumeLayout(false);
-            this.PerformLayout();
+			this.loginTextBox = new System.Windows.Forms.TextBox();
+			this.passTextBox = new System.Windows.Forms.TextBox();
+			this.label1 = new System.Windows.Forms.Label();
+			this.label2 = new System.Windows.Forms.Label();
+			this.InputBtn = new System.Windows.Forms.Button();
+			this.ExitBtn = new System.Windows.Forms.Button();
+			this.SuspendLayout();
+			// 
+			// loginTextBox
+			// 
+			this.loginTextBox.Location = new System.Drawing.Point(33, 50);
+			this.loginTextBox.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+			this.loginTextBox.Name = "loginTextBox";
+			this.loginTextBox.Size = new System.Drawing.Size(132, 30);
+			this.loginTextBox.TabIndex = 0;
+			// 
+			// passTextBox
+			// 
+			this.passTextBox.Location = new System.Drawing.Point(33, 137);
+			this.passTextBox.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+			this.passTextBox.Name = "passTextBox";
+			this.passTextBox.PasswordChar = '*';
+			this.passTextBox.Size = new System.Drawing.Size(132, 30);
+			this.passTextBox.TabIndex = 1;
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Location = new System.Drawing.Point(73, 22);
+			this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(50, 23);
+			this.label1.TabIndex = 2;
+			this.label1.Text = "Логин";
+			// 
+			// label2
+			// 
+			this.label2.AutoSize = true;
+			this.label2.Location = new System.Drawing.Point(73, 98);
+			this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(58, 23);
+			this.label2.TabIndex = 3;
+			this.label2.Text = "Пароль";
+			// 
+			// InputBtn
+			// 
+			this.InputBtn.Location = new System.Drawing.Point(12, 191);
+			this.InputBtn.Name = "InputBtn";
+			this.InputBtn.Size = new System.Drawing.Size(75, 31);
+			this.InputBtn.TabIndex = 4;
+			this.InputBtn.Text = "Войти";
+			this.InputBtn.UseVisualStyleBackColor = true;
+			this.InputBtn.Click += new System.EventHandler(this.InputBtnClick);
+			// 
+			// ExitBtn
+			// 
+			this.ExitBtn.Location = new System.Drawing.Point(108, 191);
+			this.ExitBtn.Name = "ExitBtn";
+			this.ExitBtn.Size = new System.Drawing.Size(75, 31);
+			this.ExitBtn.TabIndex = 5;
+			this.ExitBtn.Text = "Выход";
+			this.ExitBtn.UseVisualStyleBackColor = true;
+			this.ExitBtn.Click += new System.EventHandler(this.ExitBtnClick);
+			// 
+			// Authorization
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 23F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(207, 250);
+			this.ControlBox = false;
+			this.Controls.Add(this.ExitBtn);
+			this.Controls.Add(this.InputBtn);
+			this.Controls.Add(this.label2);
+			this.Controls.Add(this.label1);
+			this.Controls.Add(this.passTextBox);
+			this.Controls.Add(this.loginTextBox);
+			this.Font = new System.Drawing.Font("Segoe Print", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+			this.Name = "Authorization";
+			this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+			this.Load += new System.EventHandler(this.Authorization_Load);
+			this.ResumeLayout(false);
+			this.PerformLayout();
 
         }
 
@@ -122,5 +123,5 @@
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Button InputBtn;
         private System.Windows.Forms.Button ExitBtn;
-    }
+	}
 }
--- LoadProject/Authorization.cs
+++ LoadProject/Authorization.cs
@@ -3,7 +3,10 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
+using System.Reflection;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -15,9 +18,12 @@ namespace LoadProject
         private string _login;
         private string _password;
         MainForm mainForm;
+        public string key = "b14ca5898a4e4133bbce2ea2315a1916";
         public Authorization()
         {
-            InitializeComponent();
+             InitializeComponent();
+             writeToFile("login", Encryption("pass"));
+
         }
 
         private void ExitBtnClick(object sender, EventArgs e)
@@ -38,9 +44,13 @@ namespace LoadProject
 
         public void CheckInputData()
         {
+
+            string readTextFromFile = readFile();
+            String[] words = readTextFromFile.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+            string pasFrFile= AesOperation.DecryptString(key, words[1]);
             _login = loginTextBox.Text;
             _password = passTextBox.Text;
-            if (_login == "login" && _password == "pass")
+            if (_login == words[0] && _password == pasFrFile)
             {
                 mainForm = new MainForm();
                 mainForm.Show();
@@ -56,5 +66,41 @@ namespace LoadProject
                 }
             }
         }
-    }
+
+		private void Authorization_Load(object sender, EventArgs e)
+		{
+
+		}
+
+        public string Encryption(string password)
+		{
+            var encryptedString = AesOperation.EncryptString(key, password);
+            return encryptedString;
+        }
+
+        public void writeToFile(string login,string password)
+		{
+            string text = login + " " + password;
+            string path = Environment.CurrentDirectory+ "\\file.txt";
+            using (FileStream fstream = new FileStream(path, FileMode.Truncate))
+            {
+                byte[] array = System.Text.Encoding.Default.GetBytes(text);
+                fstream.Write(array, 0, array.Length);
+            }
+        }
+
+        public string readFile()
+		{
+            string textFromFile = string.Empty;
+            string path = Environment.CurrentDirectory;
+            using (FileStream fstream = File.OpenRead($"{path}\\file.txt"))
+            {
+                byte[] array = new byte[fstream.Length];
+                fstream.Read(array, 0, array.Length);
+                textFromFile = System.Text.Encoding.Default.GetString(array);
+            }
+            return textFromFile;
+		}
+   
+	}
 }
